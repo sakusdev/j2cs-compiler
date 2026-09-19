e9c7791635a5b8d83c70a093bcae255b2bb6b8ff
sakusdev <105345796+sakusdev@users.noreply.github.com>
2026-09-20 02:26:49 +0900

    LANGUAGE_ARGUMENTS: implement call-frame reads and count

--- runtime/J2cs.Runtime/JsArguments.cs
+++ runtime/J2cs.Runtime/JsArguments.cs
@@ -4,4 +4,25 @@ public sealed class JsArguments
 {
     private readonly JsValue[] values;
     private JsArguments(JsValue[] values) => this.values = values;
+
+    public static JsArguments Empty() => new(Array.Empty<JsValue>());
+    public static JsArguments Create(params JsValue[] values) => new((JsValue[])values.Clone());
+
+    public static JsArguments Create(JsArguments prefix, JsValue value)
+    {
+        var next = new JsValue[prefix.values.Length + 1];
+        Array.Copy(prefix.values, next, prefix.values.Length);
+        next[^1] = value;
+        return new JsArguments(next);
+    }
+
+    public static JsValue Get(JsArguments arguments, double requestedIndex)
+    {
+        if (requestedIndex < 0 || requestedIndex > int.MaxValue || Math.Truncate(requestedIndex) != requestedIndex)
+            return JsUndefined.Value;
+        var index = (int)requestedIndex;
+        return index < arguments.values.Length ? arguments.values[index] : JsUndefined.Value;
+    }
+
+    public static double Length(JsArguments arguments) => arguments.values.Length;
 }
