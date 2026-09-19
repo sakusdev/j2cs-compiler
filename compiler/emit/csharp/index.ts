e419545eb3f38b585b2924733a16452f3102403d
sakusdev <105345796+sakusdev@users.noreply.github.com>
2026-09-20 02:24:21 +0900

    LANGUAGE_ARGUMENTS: permit JsArguments.Empty helper

--- compiler/emit/csharp/index.ts
+++ compiler/emit/csharp/index.ts
@@ -26,7 +26,7 @@ const calls = new Set(['JsValue.IsTruthy', 'JsOperators.Add', 'JsOperators.Stric
   'JsReference.DivideAssignNumber', 'JsReference.RemainderAssignNumber', 'JsReference.PrefixIncrementNumber',
   'JsReference.PostfixIncrementNumber', 'JsReference.PrefixDecrementNumber', 'JsReference.PostfixDecrementNumber',
   'JsConsole.Log', 'string.Concat', 'JsObject.Create', 'JsObject.DefineDataProperty', 'JsObject.GetProperty',
-  'JsObject.SetProperty', 'JsObject.HasOwn', 'JsArray.Create', 'JsArray.DefineElement', 'JsArray.Length', 'JsArray.Push', 'JsArguments.Get']);
+  'JsObject.SetProperty', 'JsObject.HasOwn', 'JsArray.Create', 'JsArray.DefineElement', 'JsArray.Length', 'JsArray.Push', 'JsArguments.Get', 'JsArguments.Empty']);
 function expr(e: CsExpr): string {
   switch (e.kind) {
     case 'literal': return e.repr === 'number' ? number(e.value) : e.repr === 'string' ? quote(e.value) : String(e.value);
